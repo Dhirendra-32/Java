2048a1ba22164db4f953b8eeb88488824055bd78
Dhirendra-32 <tdhirendra32@gmail.com>
2021-12-27 12:53:46 +0530

    Changed array to different one

--- Java/Arrays/Easy/QuickSort.java
+++ Java/Arrays/Easy/QuickSort.java
@@ -18,7 +18,7 @@ public class QuickSort {
         System.out.println();
     }
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 20, 4, 1, 0 };
+        int[] arr = {1, 4, 45, 6, -50, 10, 2};
         int n = arr.length;
         int[] p =QuickSortAlgo(arr,0,n-1);
         printArray(p,p.length);
