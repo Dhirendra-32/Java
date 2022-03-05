d3fca8f25c5f14b696716c95d448b57f8cfac47a
Dhirendra-32 <tdhirendra32@gmail.com>
2022-03-06 02:13:31 +0530

    Even and Odd Index Prefix Sum

---
+++ Java/Arrays/Easy/IndexedNumbersPrefixSum.java
@@ -0,0 +1,35 @@
+package Arrays.Easy;
+public class IndexedNumbersPrefixSum {
+    /*declaring same length array to find prefix even array*/
+    public static int [] buildEvenIndexPrefixSum (int []arr){
+        int [] PSe = new int[arr.length];
+        PSe[0] = arr[0];
+        for (int i = 1; i < arr.length ; i++) {
+            if (i%2==0) {
+                PSe[i]=PSe[i-1]+arr[i];
+            }
+            else{
+                PSe[i]=PSe[i-1];
+            }
+        }
+        return PSe;
+    }
+    public static int [] buildOddIndexPrefixSum(int []arr){
+        int [] PSo = new int[arr.length];
+        PSo[0] = 0;
+        for (int i = 1; i < arr.length ; i++) {
+            if (i%2==0) {
+                PSo[i]=PSo[i-1];
+            }
+            else{
+                PSo[i]=PSo[i-1]+arr[i];
+            }
+        }
+        return PSo;
+    }
+    public static void main(String[] args) {
+        int [] arr = new int[]{2,4,3,1,5};
+        int [] PSe = buildEvenIndexPrefixSum(arr);
+        QuickSort.printArray(PSe, PSe.length);
+    }
+}
